0da72e10492a82b8355f7686db271d7f464026fa
Matthew <matthew7419@gmail.com>
2023-04-19 14:53:22 -0400

    Added datacontroller to app

--- FastBreak/FastBreakApp.swift
+++ FastBreak/FastBreakApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct FastBreakApp: App {
+    @StateObject var dataController = DataController()
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
