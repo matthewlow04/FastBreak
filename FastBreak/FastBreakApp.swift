ba243295d892b5629e4e52b2e8ba30b405212553
Matthew <matthew7419@gmail.com>
2023-04-19 14:03:48 -0400

    Initial Commit

---
+++ FastBreak/FastBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastBreakApp.swift
+//  FastBreak
+//
+//  Created by Matthew Low on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct FastBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
